89ec0d9e73cfb33bb8dc9b9b2b45b4aa5965afeb
alezen9 <aj0715@live.com>
2018-10-23 03:21:58 +0200

    fixed styles

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
+import postcssModules from 'postcss-modules';
 
 import pkg from './package.json'
 
@@ -24,7 +25,7 @@ export default {
   plugins: [
     external(),
     postcss({
-      modules: true
+        extensions: [ '.css' ]
     }),
     url(),
     babel({
